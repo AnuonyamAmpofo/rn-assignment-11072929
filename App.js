4582c643fcd40ec3e12b667f62afe34d373926b2
Ampmusicsog <aaamo-mensah@st.ug.edu.gh>
2024-05-27 14:03:34 +0000

    first

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>My name is <Text style={styles.nameStyle}>Anuonyam Amo-Mensah</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '008080',
+    backgroundColor: '#008080',
     alignItems: 'center',
     justifyContent: 'center',
+
+  },
+  textStyle:{
+    fontSize:24,
+  },
+  nameStyle:{
+    fontWeight:'bold',
   },
 });
